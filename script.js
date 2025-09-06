cb9a574b4b806f7cfd4ee1cd0dd7954d94b651a2
Shajidaa <shajidaakterlopa101@gmail.com>
2025-09-06 13:26:13 +0600

    update the border

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ const displayCategory=(categories)=>{
         // console.log(category);
         const categoryBox=document.createElement('div');
         categoryBox.innerHTML=`
-                 <li id=${category.id} class="hover:border-b-4 hover:border-b-red-500 cursor-pointer">${category.title}</li>
+                 <li id=${category.id} class=" border-b-2 border-transparent hover:border-b-2  hover:border-b-red-500 cursor-pointer">${category.title}</li>
                  `
         levelContainer.append(categoryBox)
     });
@@ -159,8 +159,6 @@ const showLoading=()=>{
 
 const showError=()=>{
     newsContainer.innerHTML=`
-   
-
     <div role="alert" class="alert alert-error col-span-full">
   <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
